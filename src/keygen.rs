3ec3537c2bcc46baeb75bb8fb69b374b456317c1
Slava <slava@qey.ai>
2020-07-09 17:04:09 +0300

    Keygen tool

---
+++ src/keygen.rs
@@ -0,0 +1,22 @@
+use adnl::common::KeyOption;
+use ton_types::Result;
+     
+fn gen() -> Result<()> {
+    let keypair = KeyOption::with_type_id(KeyOption::KEY_ED25519)?;
+    println!("Keypair generated:");
+    println!(                         
+        "{{\n    \
+             \"type_id\": {},\n    \
+             \"pub_key\": \"{}\",\n    \
+             \"pvt_key\": \"{}\"\n\
+        }}",
+        KeyOption::KEY_ED25519,
+        base64::encode(keypair.pub_key()?),
+        base64::encode(keypair.pvt_key()?)
+    );        
+    Ok(())
+} 
+
+fn main() {
+    gen().unwrap_or_else(|e| println!("Keypair generation error: {}", e))
+}
