96bf8450f0f0c22f0e4c07196696c2e8aa247acb
yaroslavser <sergey.yar.1979@gmail.com>
2020-11-30 17:58:46 +0300

    WIP: implement additional commands

--- src/console.rs
+++ src/console.rs
@@ -4,7 +4,7 @@ use ton_api::{
     ton::{
         self, TLObject, 
         engine::validator::ControlQueryError,
-        rpc::engine::validator::{AddValidatorAdnlAddress, ControlQuery, GenerateKeyPair},
+        rpc::engine::validator::ControlQuery,
     }
 };
 use ton_types::{error, fail, Result};
@@ -17,7 +17,10 @@ include!("../../common/src/log.rs");
 
 trait SendReceive {
     fn send<'a>(params: impl Iterator<Item = &'a str>) -> Result<TLObject>;
-    fn receive(answer: TLObject) -> std::result::Result<(), TLObject>;
+    fn receive(answer: TLObject) -> std::result::Result<(), TLObject> {
+        answer.downcast::<ton_api::ton::engine::validator::Success>()
+            .map(|_| println!("success"))
+    }
 }
 
 trait ConsoleCommand: SendReceive {
@@ -57,46 +60,161 @@ macro_rules! commands {
 
 commands! {
     NewKeypair, "newkey", "newkey\tgenerates new key pair on server"
+    Sign, "sign", "sign <keyhash> <data>\tsigns bytestring with privkey"
+    AddValidatorPermKey, "addpermkey", "addpermkey <keyhash> <election-date> <expire-at>\tadd validator permanent key"
+    AddValidatorTempKey, "addtempkey", "addtempkey <permkeyhash> <keyhash> <expireat>\tadd validator temp key"
     AddValidatorAdnlAddr, "addvalidatoraddr", "addvalidatoraddr <permkeyhash> <keyhash> <expireat>\tadd validator ADNL addr"
+    AddAdnlAddr, "addadnl", "addadnl <keyhash> <category>\tuse key as ADNL addr"
+}
+
+fn parse_any<A>(param_opt: Option<&str>, name: &str, parse_value: impl FnOnce(&str) -> Result<A>) -> Result<A> {
+    param_opt
+        .ok_or_else(|| error!("insufficient parameters"))
+        .and_then(|value| parse_value(value))
+        .map_err(|_| error!("you must give {}", name))
+}
+
+fn parse_data(param_opt: Option<&str>, name: &str) -> Result<ton::bytes> {
+    parse_any(
+        param_opt,
+        &format!("{} in hex format", name),
+        |value| Ok(ton::bytes(hex::decode(value)?))
+    )
+}
+
+fn parse_int256(param_opt: Option<&str>, name: &str) -> Result<ton::int256> {
+    parse_any(
+        param_opt,
+        &format!("{} in hex of base64 format", name),
+        |value| {
+            let value = match value.len() {
+                44 => base64::decode(value)?,
+                64 => hex::decode(value)?,
+                length => fail!("wrong hash: {} with length: {}", value, length)
+            };
+            Ok(ton::int256(value.as_slice().try_into()?))
+        }
+    )
+}
+
+fn parse_int(param_opt: Option<&str>, name: &str) -> Result<ton::int> {
+    parse_any(param_opt, name, |value| Ok(ton::int::from_str(value)?))
+}
+
+#[cfg(test)]
+fn parse_test<A>(parse: impl FnOnce(Option<&str>, &str) -> Result<A>, param: &str) -> Result<A> {
+    parse(param.split_whitespace().next(), "test")
+}
+
+#[test]
+fn test_parse_int() {
+    assert_eq!(parse_test(parse_int, "0").unwrap(), 0);
+    assert_eq!(parse_test(parse_int, "-1").unwrap(), -1);
+    assert_eq!(parse_test(parse_int, "1600000000").unwrap(), 1600000000);
+
+    parse_test(parse_int, "qwe").expect_err("must generate error");
+    parse_int(None, "test").expect_err("must generate error");
+}
+
+#[test]
+fn test_parse_int256() {
+    let ethalon = ton::int256(base64::decode("GfgI79Xf3q7r4q1SPz7wAqBt0W6CjavuADODoz/DQE8=").unwrap().as_slice().try_into().unwrap());
+    assert_eq!(parse_test(parse_int256, "GfgI79Xf3q7r4q1SPz7wAqBt0W6CjavuADODoz/DQE8=").unwrap(), ethalon);
+    assert_eq!(parse_test(parse_int256, "19F808EFD5DFDEAEEBE2AD523F3EF002A06DD16E828DABEE003383A33FC3404F").unwrap(), ethalon);
+
+    parse_test(parse_int256, "11").expect_err("must generate error");
+    parse_int256(None, "test").expect_err("must generate error");
+}
+
+#[test]
+fn test_parse_data() {
+    let ethalon = ton::bytes(vec![10, 77]);
+    assert_eq!(parse_test(parse_data, "0A4D").unwrap(), ethalon);
+
+    parse_test(parse_data, "QQ").expect_err("must generate error");
+    parse_test(parse_data, "GfgI79Xf3q7r4q1SPz7wAqBt0W6CjavuADODoz/DQE8=").expect_err("must generate error");
+    parse_data(None, "test").expect_err("must generate error");
 }
 
 impl SendReceive for NewKeypair {
     fn send<'a>(_params: impl Iterator<Item = &'a str>) -> Result<TLObject> {
-        Ok(TLObject::new(GenerateKeyPair))
+        Ok(TLObject::new(ton::rpc::engine::validator::GenerateKeyPair))
     }
     fn receive(answer: TLObject) -> std::result::Result<(), TLObject> {
         answer.downcast::<ton_api::ton::engine::validator::KeyHash>()
-            .map(|key_hash| println!("received public key hash: {}", base64::encode(&key_hash.key_hash().0)))
+            .map(|key_hash| println!("received public key hash: {} {}",
+                hex::encode(&key_hash.key_hash().0), base64::encode(&key_hash.key_hash().0)))
     }
 }
 
-impl SendReceive for AddValidatorAdnlAddr {
+impl SendReceive for Sign {
     fn send<'a>(mut params: impl Iterator<Item = &'a str>) -> Result<TLObject> {
-        let permanent_key_hash =  ton::int256(params.next()
-            .ok_or_else(|| error!("insufficient parameters"))
-            .and_then(|hash| Ok(base64::decode(hash)?))
-            .and_then(|hash| Ok(hash.as_slice().try_into()?))
-            .map_err(|_| error!("you must give permanent_key_hash in format"))?
-        );
-        let key_hash =  ton::int256(params.next()
-            .ok_or_else(|| error!("insufficient parameters"))
-            .and_then(|hash| Ok(base64::decode(hash)?))
-            .and_then(|hash| Ok(hash.as_slice().try_into()?))
-            .map_err(|_| error!("you must give key_hash in base64 format"))?
-        );
-        let ttl = params.next()
-            .ok_or_else(|| error!("insufficient parameters"))
-            .and_then(|ttl| Ok(ton::int::from_str(ttl)?))
-            .map_err(|_| error!("you must give expire_at"))?;
-        Ok(TLObject::new(AddValidatorAdnlAddress {
+        let key_hash = parse_int256(params.next(), "key_hash")?;
+        let data = parse_data(params.next(), "data")?;
+        Ok(TLObject::new(ton::rpc::engine::validator::Sign {
+            key_hash,
+            data
+        }))
+    }
+    fn receive(answer: TLObject) -> std::result::Result<(), TLObject> {
+        answer.downcast::<ton_api::ton::engine::validator::Signature>()
+            .map(|signature| println!("got signature: {} {}",
+                hex::encode(&signature.signature().0), base64::encode(&signature.signature().0)))
+    }
+}
+
+impl SendReceive for AddValidatorPermKey {
+    fn send<'a>(mut params: impl Iterator<Item = &'a str>) -> Result<TLObject> {
+        let key_hash =  parse_int256(params.next(), "key_hash")?;
+        let election_date = parse_int(params.next(), "election_date")?;
+        let ttl = parse_int(params.next(), "expire_at")? - election_date;
+        Ok(TLObject::new(ton::rpc::engine::validator::AddValidatorPermanentKey {
+            key_hash,
+            election_date,
+            ttl
+        }))
+    }
+}
+
+impl SendReceive for AddValidatorTempKey {
+    fn send<'a>(mut params: impl Iterator<Item = &'a str>) -> Result<TLObject> {
+        let now = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs() as ton::int;
+        let permanent_key_hash = parse_int256(params.next(), "permanent_key_hash")?;
+        let key_hash = parse_int256(params.next(), "key_hash")?;
+        let ttl = parse_int(params.next(), "expire_at")? - now;
+        Ok(TLObject::new(ton::rpc::engine::validator::AddValidatorTempKey {
             permanent_key_hash,
             key_hash,
             ttl
         }))
     }
-    fn receive(answer: TLObject) -> std::result::Result<(), TLObject> {
-        answer.downcast::<ton_api::ton::engine::validator::KeyHash>()
-            .map(|key_hash| println!("received public key hash: {}", base64::encode(&key_hash.key_hash().0)))
+}
+
+impl SendReceive for AddValidatorAdnlAddr {
+    fn send<'a>(mut params: impl Iterator<Item = &'a str>) -> Result<TLObject> {
+        let now = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs() as ton::int;
+        let permanent_key_hash = parse_int256(params.next(), "permanent_key_hash")?;
+        let key_hash = parse_int256(params.next(), "key_hash")?;
+        let ttl = parse_int(params.next(), "expire_at")? - now;
+        Ok(TLObject::new(ton::rpc::engine::validator::AddValidatorAdnlAddress {
+            permanent_key_hash,
+            key_hash,
+            ttl
+        }))
+    }
+}
+
+impl SendReceive for AddAdnlAddr {
+    fn send<'a>(mut params: impl Iterator<Item = &'a str>) -> Result<TLObject> {
+        let key_hash = parse_int256(params.next(), "key_hash")?;
+        let category = parse_int(params.next(), "category")?;
+        if category < 0 || category > 15 {
+            fail!("category must be not negative and less than 16")
+        }
+        Ok(TLObject::new(ton::rpc::engine::validator::AddAdnlId {
+            key_hash,
+            category
+        }))
     }
 }
 
@@ -126,7 +244,9 @@ impl LiteClient {
         let boxed = ControlQuery {
             data: ton::bytes(serialize(&query)?)
         };
-        match self.adnl.query(&TLObject::new(boxed)).await?.downcast::<ControlQueryError>() {
+        let answer = self.adnl.query(&TLObject::new(boxed)).await
+            .map_err(|err| error!("Error receiving answer: {}", err))?;
+        match answer.downcast::<ControlQueryError>() {
             Err(answer) => match command_receive(name, answer) {
                 Err(answer) => fail!("Wrong response to {:?}: {:?}", query, answer),
                 Ok(()) => Ok(())
@@ -210,7 +330,6 @@ async fn main() {
             }).into());
         }
         serde_json::json!(map).to_string()
-        // format!("", serde_json::json!(map));
     };
     let config = AdnlClientConfig::from_json(&config).unwrap();
     let timeout = match args.value_of("TIMEOUT") {
@@ -222,7 +341,9 @@ async fn main() {
     if let Some(commands) = args.values_of("COMMANDS") {
         // batch mode - call commands and exit
         for command in commands {
-            client.command(command.trim_matches('\"')).await.unwrap();
+            if let Err(err) = client.command(command.trim_matches('\"')).await {
+                println!("Error executing comamnd: {}", err);
+            }
             tokio::time::delay_for(timeout).await;
         }
     } else {
