d0691c47ee9da0fb6ba6c43b985521bf7fef9785
yaroslavser <sergey.yar.1979@gmail.com>
2020-12-12 19:46:23 +0300

    additional prints in keygen

--- bin/keygen.rs
+++ bin/keygen.rs
@@ -5,14 +5,12 @@ fn gen() -> Result<()> {
     let (private, public) = KeyOption::with_type_id(KeyOption::KEY_ED25519)?;
     println!("Keypair generated:");
     println!("{}", serde_json::to_string_pretty(&private)?);
-    println!(                         
-        "{{\n    \
-             \"type_id\": {},\n    \
-             \"pub_key\": \"{}\",\n\
-        }}",
-        KeyOption::KEY_ED25519,
-        base64::encode(public.pub_key()?),
-    );
+    println!("{:#}", serde_json::json!({
+        "type_id": KeyOption::KEY_ED25519,
+        "pub_key": base64::encode(public.pub_key()?),
+        "keyhash": base64::encode(public.id().data()),
+        "pub_key_hex": hex::encode(public.pub_key()?)
+    }));
     Ok(())
 } 
 
