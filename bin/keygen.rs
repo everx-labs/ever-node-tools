c2afc41f268bddb1bc1e40ce7f92c0357ae68caf
yaroslavser <sergey.yar.1979@gmail.com>
2020-12-15 12:24:47 +0300

    WIP: change keygen output

--- bin/keygen.rs
+++ bin/keygen.rs
@@ -4,10 +4,16 @@ use ton_types::Result;
 fn gen() -> Result<()> {
     let (private, public) = KeyOption::with_type_id(KeyOption::KEY_ED25519)?;
     println!("Keypair generated:");
-    println!("{}", serde_json::to_string_pretty(&private)?);
+    let private = serde_json::to_value(private)?;
     println!("{:#}", serde_json::json!({
-        "type_id": KeyOption::KEY_ED25519,
-        "pub_key": base64::encode(public.pub_key()?),
+        "private": {
+            "type_id": KeyOption::KEY_ED25519,
+            "pvt_key": private["pvt_key"],
+        },
+        "public": {
+            "type_id": KeyOption::KEY_ED25519,
+            "pub_key": base64::encode(public.pub_key()?),
+        },
         "keyhash": base64::encode(public.id().data()),
         "pub_key_hex": hex::encode(public.pub_key()?)
     }));
