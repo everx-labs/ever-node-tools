3ddce62e26cd705643139e8d723b8a14f9739ced
yaroslavser <sergey.yar.1979@gmail.com>
2021-01-22 15:52:49 +0300

    print brief info of block and state

--- bin/print.rs
+++ bin/print.rs
@@ -4,19 +4,35 @@ use ton_block::{AccountIdPrefixFull, BlockIdExt, Block, Deserializable, ShardSta
 use ton_node::db::{InternalDb, InternalDbConfig, InternalDbImpl};
 use ton_types::Result;
 
-async fn print_block(db: &InternalDbImpl, block_id: BlockIdExt) -> Result<()> {
-    println!("loading block: {}", block_id);
-    let handle = db.load_block_handle(&block_id)?;
-    let block = db.load_block_data(&handle).await?;
-    println!("{}", ton_block_json::debug_block(block.block().clone())?);
+fn print_block(block: &Block, brief: bool) -> Result<()> {
+    if brief {
+        println!("{}", ton_block_json::debug_block(block.clone())?);
+    } else {
+        println!("{}", ton_block_json::debug_block_full(block)?);
+    }
     Ok(())
 }
 
-async fn print_state(db: &InternalDbImpl, block_id: BlockIdExt) -> Result<()> {
+fn print_state(state: &ShardStateUnsplit, brief: bool) -> Result<()> {
+    if brief {
+        println!("{}", ton_block_json::debug_state(state.clone())?);
+    } else {
+        println!("{}", ton_block_json::debug_state_full(state.clone())?);
+    }
+    Ok(())
+}
+
+async fn print_db_block(db: &InternalDbImpl, block_id: BlockIdExt, brief: bool) -> Result<()> {
+    println!("loading block: {}", block_id);
+    let handle = db.load_block_handle(&block_id)?;
+    let block = db.load_block_data(&handle).await?;
+    print_block(block.block(), brief)
+}
+
+async fn print_db_state(db: &InternalDbImpl, block_id: BlockIdExt, brief: bool) -> Result<()> {
     println!("loading state: {}", block_id);
     let state = db.load_shard_state_dynamic(&block_id)?;
-    println!("{}", ton_block_json::debug_state(state.shard_state().clone())?);
-    Ok(())
+    print_state(state.shard_state(), brief)
 }
 
 async fn print_shards(db: &InternalDbImpl, block_id: BlockIdExt) -> Result<()> {
@@ -79,14 +95,19 @@ async fn main() -> Result<()> {
             .help("print containtment of bag of cells")
             .takes_value(true)
             .number_of_values(1))
+        .arg(Arg::with_name("BRIEF")
+            .short("i")
+            .long("brief")
+            .help("print brief info (block without messages and transactions, state without accounts) "))
         .get_matches();
 
+    let brief = args.is_present("BRIEF");
     if let Some(path) = args.value_of("BOC") {
         let bytes = std::fs::read(path)?;
         if let Ok(block) = Block::construct_from_bytes(&bytes) {
-            println!("{}", ton_block_json::debug_block(block)?);
+            print_block(&block, brief)?;
         } else if let Ok(state) = ShardStateUnsplit::construct_from_bytes(&bytes) {
-            println!("{}", ton_block_json::debug_state(state)?);
+            print_state(&state, brief)?;
         }
     } else if let Some(db_dir) = args.value_of("PATH") {
         let db_config = InternalDbConfig { db_directory: db_dir.to_string() };
@@ -94,11 +115,11 @@ async fn main() -> Result<()> {
 
         if let Some(block_id) = args.value_of("BLOCK") {
             let block_id = get_block_id(&db, block_id)?;
-            print_block(&db, block_id).await?;
+            print_db_block(&db, block_id, brief).await?;
         }
         if let Some(block_id) = args.value_of("STATE") {
             let block_id = get_block_id(&db, block_id)?;
-            print_state(&db, block_id).await?;
+            print_db_state(&db, block_id, brief).await?;
         }
         if let Some(block_id) = args.value_of("SHARDS") {
             let block_id = get_block_id(&db, block_id)?;
