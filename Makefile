9d32416bb889343b067b384d9dce7af4c83390a5
Slava <slava@qey.ai>
2021-06-01 11:28:09 +0300

    Changes in ping utility

--- Makefile
+++ Makefile
@@ -10,10 +10,13 @@ key: Makefile
 keyid: Makefile 
 #	@cargo run --release --bin keyid pvt 3pdYMu6fo8IMq1PBaVDHKrFSBrSK/M/+85CXIwCkz5g=
 #	@cargo run --release --bin keyid pvt 9Pk32/BmvPGbhqXVyXwHruhVRZzoqygEsCII2vdPt1k=
-	@cargo run --release --bin keyid pvt z4Vtl5dUEgCYwvErV3It6BjL3WZboYGjQDPexcgGJpc=
+	@cargo run --release --bin keyid pvt tLO00fTB/83cSd744Gp43fmJG896HN/WnUmN/8RlGZE=
 
 dht: Makefile 
 	@cargo run --release --bin dhtscan ton-global.config-mainet.json --jsonl
 
 tst: Makefile 
 	@cargo run --release --bin notests ../notests
+
+ping: Makefile 
+	@cargo run --release --bin adnl_ping p+9/xO85UFX58sWtPf0JaQMcq5khjKAzCqTYjnAdKWE= 51.210.214.159:30307 ..\configs\ton-global.config-mainet.json
--- bin/adnl_ping.rs
+++ bin/adnl_ping.rs
@@ -1,6 +1,7 @@
-use adnl::{common::KeyOption, node::{AdnlNode, AdnlNodeConfig, IpAddress}};
+use adnl::{common::KeyOption, node::{AdnlNode, AdnlNodeConfig, AdnlNodeConfigJson, IpAddress}};
 use overlay::OverlayNode;
 use std::{convert::TryInto, env, fs::File, io::BufReader, sync::Arc};
+use ton_api::ton::{TLObject, rpc::ton_node::GetCapabilities};
 use ton_node::config::TonNodeGlobalConfigJson;
 use ton_types::{error, fail, Result};
 
@@ -9,23 +10,38 @@ include!("../common/src/test.rs");
 const IP: &str = "0.0.0.0:4191";
 const KEY_TAG: usize = 2;
 
-fn ping(pub_key: &str, ip_addr: &str, cfgfile: &str) -> Result<()> {
-
+fn read_config<T: for<'de> serde::Deserialize<'de>>(cfgfile: &str, cfgtype: &str) -> Result<T> {
     let file = File::open(cfgfile)?;
     let reader = BufReader::new(file);
-    let config: TonNodeGlobalConfigJson = serde_json::from_reader(reader).map_err(
-        |e| error!("Cannot read config from file {}: {}", cfgfile, e) 
-    )?;
-    let zero_state_file_hash = config.zero_state()?.file_hash.as_slice().clone();
+    serde_json::from_reader(reader).map_err(
+        |e| error!("Cannot read {} config from file {}: {}", cfgtype, cfgfile, e) 
+    )
+}
+
+fn ping(
+    pub_key: &str, 
+    ip_addr: &str, 
+    global_cfgfile: &str, 
+    local_cfgfile: Option<&str>
+) -> Result<()> {
+
+    let global_cfg: TonNodeGlobalConfigJson = read_config(global_cfgfile, "global")?;
+    let zero_state_file_hash = global_cfg.zero_state()?.file_hash.as_slice().clone();
     let ip = IpAddress::from_string(ip_addr)?;
 
     let rt = tokio::runtime::Runtime::new()?;
-    let (_, config) = AdnlNodeConfig::with_ip_address_and_key_type(
-        IP, 
-        KeyOption::KEY_ED25519, 
-        vec![KEY_TAG]
-    )?;
-    let adnl = rt.block_on(AdnlNode::with_config(config))?;
+    let local_cfg = if let Some(local_cfgfile) = local_cfgfile {
+        let local_cfg: AdnlNodeConfigJson = read_config(local_cfgfile, "local")?;
+        AdnlNodeConfig::from_json_config(&local_cfg, true)?
+    } else {
+        let (_, local_cfg) = AdnlNodeConfig::with_ip_address_and_key_type(
+            IP, 
+            KeyOption::KEY_ED25519, 
+            vec![KEY_TAG]
+        )?;
+        local_cfg
+    };
+    let adnl = rt.block_on(AdnlNode::with_config(local_cfg))?;
     let overlay = OverlayNode::with_adnl_node_and_zero_state(
         adnl.clone(), 
         &zero_state_file_hash,
@@ -58,20 +74,44 @@ fn ping(pub_key: &str, ip_addr: &str, cfgfile: &str) -> Result<()> {
     if let Some(reply) = rt.block_on(overlay.get_random_peers(&other_id, &overlay_id, None))? {
         println!("Got response: {:?}", reply)
     } else {
-        fail!("No response to ping")
+        println!("No response to ping")
     }
+
+    println!("Pinging {}/{} by GetCapabilities", other_id, ip_addr);
+    let query = TLObject::new(GetCapabilities);
+    if let Some(reply) = rt.block_on(overlay.query(&other_id, &query, &overlay_id, None))? {
+        println!("Got response: {:?}", reply)
+    } else {
+        println!("No response to ping")
+    }
+
     Ok(())
 
 }
 
+fn print_usage() {
+    println!(
+        "Usage: adnl_ping <pub-key> <ip-addr> <path-to-global-config> \
+        [--config <path-to-local-config>]"
+    )
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 4 {
-        println!("Usage: adnl_ping <pub-key> <ip-addr> <path-to-global-config>");
+    let local_config = if args.len() == 6 {
+        if args[4].as_str() != "--config" {
+            print_usage();
+            return
+        }    
+        Some(args[5].as_str())
+    } else if args.len() == 4 {
+        None
+    } else {
+        print_usage();
         return
     };
     init_log("./common/config/log_cfg.yml");
-    ping(args[1].as_str(), args[2].as_str(), args[3].as_str()).unwrap_or_else(
+    ping(args[1].as_str(), args[2].as_str(), args[3].as_str(), local_config).unwrap_or_else(
         |e| println!("ADNL pinging error: {}", e)
     )
 }
